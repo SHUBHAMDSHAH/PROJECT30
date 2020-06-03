da5cdf3afd8cea737011a6137b1df2bc3509ae9b
SHUBHAMDSHAH <63915369+SHUBHAMDSHAH@users.noreply.github.com>
2020-06-03 21:51:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var block8,block9,block10,block11,block12,block13,block14,block15,block16;
+
+function setup() {
+ 
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+
+  //top
+  block16 = new Block(390,155,30,40);
+  createCanvas(800,400);
+}
+
+function draw() {
+    
+
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+  background(0);
+  drawSprites();
+}
